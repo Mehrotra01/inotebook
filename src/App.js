368da70927332dd12db48654bcfccc462a533f95
Harsh Mehrotra <harshmehrotra01@gmail.com>
2023-08-31 03:32:46 +0530

    adding notes fetch

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from "react";
-
+import { Routes, Route } from "react-router-dom";
 import NavBar from "./components/Navbar";
 import About from "./components/About";
 import Home from "./components/Home";
+import NoteState from "./context/notes/noteState";
 
 function App() {
   return (
     <>
-      {/* <Router> */}
+      <NoteState>
         <NavBar />
-        <Home/>
-          {/* <Route exact path="/about">
-         <> <About /></>
-          </Route>
-          </Routes>
-        </Router> */}
+        <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+        </div>
+      </NoteState>
     </>
   );
 }
--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import noteContext from "../context/notes/noteContext";
 
 const About = () => {
-  return <div>This is About of iNotebook</div>;
+  const heyy = useContext(noteContext);
+  useEffect(() => {
+    heyy.update();
+  }, [12,12]);
+  return (
+    <>
+      <h3>This is About of iNotebook</h3>
+      <hr />
+      <p>This is About {heyy.state.name} and he is in cource {heyy.state.cource}</p>
+    </>
+  );
 };
 
 export default About;
